6205d92f386114d53e6f127c48ac10191228d1e8
rydeblack <ryhad.boughanmi@gmail.com>
2024-11-24 23:11:10 +0100

    updated scroll and footer

--- js/app.js
+++ js/app.js
@@ -7,27 +7,19 @@ $(document).ready(function() {
     offset: 50
   });
 
-  // Add smooth scrolling on all links inside the navbar
+  
   $("a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
-
-      // Store hash
       var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
+      $('html, body, .parallax-stars').animate({
+        scrollTop: $(hash).offset().top - 50
       }, 800, function() {
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     }
   });
+  
 
 });
 
@@ -40,6 +32,9 @@ $(window).scroll(function() {
       $('.opaque-navbar').removeClass('opaque');
   }
 });
+
+
+
 
 
 // Back to top button
